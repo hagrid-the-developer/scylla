08a616f599174d7c53fa80b90fb996ff3406d621
Daniel Fiala <freelancer@meave.biz>
2017-11-02 13:59:03 +0100

    Added tests for division of big_decimal.

--- configure.py
+++ configure.py
@@ -639,6 +639,7 @@ pure_boost_tests = set([
     'tests/vint_serialization_test',
     'tests/compress_test',
     'tests/chunked_vector_test',
+    'tests/big_decimal_test',
 ])
 
 tests_not_using_seastar_test_framework = set([
