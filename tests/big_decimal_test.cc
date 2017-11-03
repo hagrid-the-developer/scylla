1df8463f433600af4e7068f882ad9b02a1b36379
Daniel Fiala <freelancer@meave.biz>
2017-11-03 11:29:46 +0100

    Added other tests for big_decimal, test for scale as well.

--- tests/big_decimal_test.cc
+++ tests/big_decimal_test.cc
@@ -49,15 +49,45 @@ namespace {
 void test_div(const char *r_cstr, const int64_t q, const char *expected_cstr) {
     big_decimal r{r_cstr};
     auto res = r / q;
-    BOOST_REQUIRE_EQUAL(res.to_string(), expected_cstr);
+    big_decimal expected{expected_cstr};
+    BOOST_REQUIRE_EQUAL(res.unscaled_value(), expected.unscaled_value());
+    BOOST_REQUIRE_EQUAL(res.scale(), expected.scale());
+}
+
+void test_assignadd(const char *x_cstr, const char *y_cstr, const char *expected_cstr) {
+    big_decimal x{x_cstr};
+    big_decimal y{y_cstr};
+    big_decimal expected{expected_cstr};
+    x +=  y;
+    BOOST_REQUIRE_EQUAL(x.unscaled_value(), expected.unscaled_value());
+    BOOST_REQUIRE_EQUAL(x.scale(), expected.scale());
 }
 
 } /* anonymous namespoce */
 
+BOOST_AUTO_TEST_CASE(test_big_decimal_construct_from_string) {
+    big_decimal x0{"0"};
+    big_decimal x1{"0.0"};
+    big_decimal x2{"0.00"};
+    big_decimal x3{"0.000"};
+
+    BOOST_REQUIRE_EQUAL(x0.unscaled_value(), 0);
+    BOOST_REQUIRE_EQUAL(x0.scale(), 0);
+
+    BOOST_REQUIRE_EQUAL(x1.unscaled_value(), 0);
+    BOOST_REQUIRE_EQUAL(x1.scale(), 1);
+
+    BOOST_REQUIRE_EQUAL(x2.unscaled_value(), 0);
+    BOOST_REQUIRE_EQUAL(x2.scale(), 2);
+
+    BOOST_REQUIRE_EQUAL(x3.unscaled_value(), 0);
+    BOOST_REQUIRE_EQUAL(x3.scale(), 3);
+}
+
 BOOST_AUTO_TEST_CASE(test_big_decimal_div) {
     test_div("1", 4, "0");
     test_div("1.00", 4, "0.25");
-    test_div("1.000", 4, "0.25");
+    test_div("1.000", 4, "0.250");
     test_div("1", 3, "0");
     test_div("1.00", 3, "0.33");
     test_div("1.000", 3, "0.333");
@@ -69,7 +99,7 @@ BOOST_AUTO_TEST_CASE(test_big_decimal_div) {
 
     test_div("-1", 4, "0");
     test_div("-1.00", 4, "-0.25");
-    test_div("-1.000", 4, "-0.25");
+    test_div("-1.000", 4, "-0.250");
     test_div("-1", 3, "0");
     test_div("-1.00", 3, "-0.33");
     test_div("-1.000", 3, "-0.333");
@@ -78,4 +108,13 @@ BOOST_AUTO_TEST_CASE(test_big_decimal_div) {
     test_div("-16", 10, "-2");
     test_div("-25", 10, "-2");
     test_div("-26", 10, "-3");
+}
+
+BOOST_AUTO_TEST_CASE(test_big_decimal_assignadd) {
+    test_assignadd("1", "4", "5");
+    test_assignadd("1.00", "4.00", "5.00");
+    test_assignadd("1.000", "4.000", "5.000");
+    test_assignadd("1", "-1", "0");
+    test_assignadd("1.00", "-1.00", "0.00");
+    test_assignadd("1.000", "-1.000", "0.000");
 }
